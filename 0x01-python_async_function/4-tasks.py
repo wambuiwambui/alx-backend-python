a21f1777af00db8b402bbb0b2f4d49645afba804
wambuiwambui <tnjoki11@gmail.com>
2023-07-10 16:21:03 +0300

    done

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python3
-"""task 4"""
+"""
+task 4
+"""
+
 import asyncio
 from typing import List
+from random import uniform
 
 
-task_wait_random = __import__('3-tasks').task_wait_random
-wait_random = __import__('0-basic_async_syntax').wait_random
+async def task_wait_random(max_delay: int) -> float:
+    """Asynchronous function that waits for a random delay."""
+    delay = uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """return a list of all the delays"""
-    delays_list = await asyncio.gather(
-        *tuple(map(lambda _: task_wait_random(max_delay), range(n))))
-    return sorted(delays_list)
+    """
+    Asynchronous function that returns a list of all the delays.
+
+    Args:
+        n (int): The number of tasks.
+        max_delay (int): The maximum delay.
+
+    Returns:
+        List[float]: A list of all the delays.
+
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
